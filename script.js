b3455a7c47dc20eb7e2b219e6902d46192a22b29
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-07-17 11:56:46 -0300

    coded the selection script and did some tests

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-//import 'jquery'
+import 'jquery'
 
 $(document).ready(function() {
+    $("div#container").focus()
     $('button.toolbar-btn').click(function() {
         var data = this && $(this).data && $(this).data() // returns the last one if all the previous are truthy, this is basically a verification, all i want is $(this).data()
         if (data && data.format && document.execCommand) { // `this` in the context is the element being clicked
             document.execCommand(data.format, false, null)
             $('div#container').focus()
-            console.log("this: ", this)
-            console.log("$(this).data: ", $(this).data)
-            console.log("$(this).data(): ", $(this).data())
         }
     })
-
     $('select.toolbar-ddl').change(function() {
         var data = this && $(this).data && $(this).data()
         if (data && data.format && document.execCommand) {
@@ -20,4 +17,43 @@ $(document).ready(function() {
             $('div#container').focus()
         }
     })
+    $("button#btnCreateSelection").click(function() {
+        var container = document.getElementById("container")
+        container.innerHTML = "Here is some sample text for selection"
+        var range = document.createRange()
+        range.setStart(container.firstChild, 5)
+        range.setEnd(container.firstChild, 17)
+        setSelectionRange(range)
+    })
+    $("button#btnStoreSelection").click(function() {
+        window.selectedRange = getSelectionRange()
+    })
+    $("button#btnRestoreSelection").click(function() {
+        if (window.selectedRange) {
+            setSelectionRange(window.selectedRange)
+        }
+    })
 })
+
+function getSelectionRange() {
+    if (window.getSelection) {
+        var sel = window.getSelection()
+        if (sel.getRangeAt && sel.rangeCount) {
+            return sel.getRangeAt(0)
+        } else {
+            var range = document.createRange()
+            range.setStart(sel.anchorNode, sel.anchorOffset)
+            range.setEnd(sel.focusNode, sel.focusOffset)
+            return range
+        }
+    }
+    return null
+}
+
+function setSelectionRange(range) {
+    if (range && window.getSelection) {
+        var sel = window.getSelection()
+        sel.removeAllRanges()
+        sel.addRange(range)
+    }
+}
