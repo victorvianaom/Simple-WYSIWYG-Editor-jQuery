b4d802d1a61e0821792e703a3bf176ad52c480e5
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-07-16 21:31:31 -0300

    using the data.format and execCommand to apply to styles

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-$(document).ready
+//import 'jquery'
+
+$(document).ready(function() {
+    $('button.toolbar-btn').click(function() {
+        var data = this && $(this).data && $(this).data()
+        if (data && data.format && document.execCommand) {
+            document.execCommand(data.format, false, null)
+            $('div#container').focus()
+        }
+    })
+
+    $('select.toolbar-ddl').change(function() {
+        var data = this && $(this).data && $(this).data()
+        if (data && data.format && document.execCommand) {
+            document.execCommand(data.format, false, this[this.selectedIndex].value)
+            this.selectedIndex = 0
+            $('div#container').focus()
+        }
+    })
+})
