70ac2cf1a565a0f4ed8f2236bd61d12870d06d14
victorvianaom <35640700+victorvianaom@users.noreply.github.com>
2020-07-16 22:33:17 -0300

    created a single page, and did some jQuery tests

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 $(document).ready(function() {
     $('button.toolbar-btn').click(function() {
-        var data = this && $(this).data && $(this).data()
-        if (data && data.format && document.execCommand) {
+        var data = this && $(this).data && $(this).data() // returns the last one if all the previous are truthy, this is basically a verification, all i want is $(this).data()
+        if (data && data.format && document.execCommand) { // `this` in the context is the element being clicked
             document.execCommand(data.format, false, null)
             $('div#container').focus()
+            console.log("this: ", this)
+            console.log("$(this).data: ", $(this).data)
+            console.log("$(this).data(): ", $(this).data())
         }
     })
 
